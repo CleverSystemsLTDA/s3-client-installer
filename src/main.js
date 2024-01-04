afb762c29b6b2b857969f83124eef98973e4e06f
Rafael Almeida <rafael.almeida@cleversystems.com.br>
2024-01-04 16:10:48 -0300

    [master] - versao 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-const {
-	app,
-	shell,
-	ipcMain,
-	dialog,
-	BrowserWindow,
-} = require("electron");
-const {
-	autoUpdater,
-} = require("electron-updater");
+const { app, shell, ipcMain, dialog, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
 const { resolve, join } = require("path");
 
 let mainWindow;
@@ -20,62 +12,62 @@ let mainWindow;
 // 	}, 10000);
 // }
 
-const path = join(
-	process.resourcesPath,
-	"s3client.exe"
-);
+const path = join(process.resourcesPath, "s3client.exe");
 
 function createWindow() {
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		show: false,
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 }
 
 ipcMain.on("app_version", (event) => {
-	event.sender.send("app_version", {
-		version: app.getVersion(),
-	});
+  event.sender.send("app_version", {
+    version: app.getVersion(),
+  });
 });
 
 app.on("ready", () => {
-	autoUpdater.autoDownload = false;
-	autoUpdater.autoInstallOnAppQuit = false;
-	autoUpdater.checkForUpdatesAndNotify();
-	createWindow();
-	shell.openPath(resolve(path));
+  autoUpdater.autoDownload = false;
+  autoUpdater.autoInstallOnAppQuit = false;
+  autoUpdater.checkForUpdatesAndNotify();
+  createWindow();
+  shell.openPath(resolve(path));
 });
 
 autoUpdater.on("update-available", () => {
-	dialog
-		.showMessageBox(mainWindow, {
-			type: "question",
-			title: "Atualização Disponível",
-			message:
-				"Uma nova atualização está disponível. Deseja instalá-la agora?",
-			buttons: ["Sim", "Não"],
-		})
-		.then((result) => {
-			if (result.response === 0) {
-				autoUpdater.downloadUpdate();
-			}
-		});
+  dialog
+    .showMessageBox(mainWindow, {
+      type: "question",
+      title: "Atualização Disponível",
+      message: "Uma nova atualização está disponível. Deseja instalá-la agora?",
+      buttons: ["Sim", "Não"],
+    })
+    .then((result) => {
+      if (result.response === 0) {
+        autoUpdater.downloadUpdate();
+      }
+    });
 });
 
 autoUpdater.on("update-downloaded", () => {
-	dialog.showMessageBox(mainWindow, {
-		type: "info",
-		title: "Atualização baixada",
-		message:
-			"A atualização foi baixada. Reinicie a aplicação para aplicar as mudanças.",
-		buttons: ["OK"],
-	});
+  dialog
+    .showMessageBox(mainWindow, {
+      type: "info",
+      title: "Atualização baixada",
+      message:
+        "A atualização foi baixada. Reinicie a aplicação para aplicar as mudanças.",
+      buttons: ["Restart", "Depois"],
+    })
+    .then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
 });
 
 ipcMain.on("restart_app", () => {
-	autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall();
 });
