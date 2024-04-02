5f2152103a972a772e8e66f2d7c0e447e6a85ad8
Thayllan Felipe Monteiro Siqueira <thayllan.siqueira@cleversystems.com.br>
2024-04-02 12:14:55 -0300

    [s3client-installer-2.3.0] - Nova versão 2.3.0 - Correções Channels

--- src/main.js
+++ src/main.js
@@ -10,14 +10,6 @@ let mainWindow;
 let child = null;
 let downloadPercent = 0;
 
-/* if (process.env.NODE_ENV === "development") {
-  autoUpdater.autoDownload = false;
-  autoUpdater.autoInstallOnAppQuit = false;
-  setInterval(() => {
-    autoUpdater.checkForUpdates();
-  }, 10000);
-} */
-
 const extraPath = join(process.resourcesPath, "..");
 
 const path = join(extraPath, "application.exe");
@@ -60,35 +52,43 @@ async function checkUpdate() {
 }
 
 function updaterListeners() {
-  autoUpdater.on("update-available", () => {
-    dialog
-      .showMessageBox(mainWindow, {
+  autoUpdater.on("update-available", (info) => {
+    /* log.info('update-available: ');
+    log.info(info); */
+    const arrVersion = info.version.split('-');
+    const updateChannel = arrVersion[1];
+
+    if (updateChannel === autoUpdater.channel) {
+      dialog.showMessageBox(mainWindow, {
         type: "question",
-        title: "Atualização Disponível",
+        title: `Atualização Disponível (${info.version})`,
         message:
           "Uma nova atualização está disponível. Deseja instalá-la agora?",
         buttons: ["Sim", "Não"],
       })
-      .then((result) => {
-        if (result.response === 0) {
-          autoUpdater.downloadUpdate();
-          checkUpdate();
-        }
-        if (result.response === 1) {
-          openApplication();
-        }
-      });
+        .then((result) => {
+          if (result.response === 0) {
+            autoUpdater.downloadUpdate();
+            checkUpdate();
+          }
+          if (result.response === 1) {
+            openApplication();
+          }
+        });
+
+    } else {
+      openApplication();
+    }
   });
 
   autoUpdater.on("update-downloaded", () => {
-    dialog
-      .showMessageBox(mainWindow, {
-        type: "info",
-        title: "Atualização baixada",
-        message:
-          "A atualização foi baixada. Reinicie a aplicação para aplicar as mudanças.",
-        buttons: ["Reniciar", "Depois"],
-      })
+    dialog.showMessageBox(mainWindow, {
+      type: "info",
+      title: "Atualização baixada",
+      message:
+        "A atualização foi baixada. Reinicie a aplicação para aplicar as mudanças.",
+      buttons: ["Reniciar", "Depois"],
+    })
       .then((returnValue) => {
         if (returnValue.response === 0) autoUpdater.quitAndInstall(true, true);
       });
@@ -117,20 +117,26 @@ function openApplication() {
 }
 
 app.whenReady().then(async () => {
+  log.info('App starting...');
   autoUpdater.autoDownload = false;
   autoUpdater.autoInstallOnAppQuit = false;
+  autoUpdater.allowPrerelease = true;
   autoUpdater.allowDowngrade = true;
-  autoUpdater.channel = 'alpha';
+  autoUpdater.channel = 'beta';
 
   autoUpdater.logger = log;
   autoUpdater.logger.transports.file.level = 'info';
-  log.info('App starting...');
-  log.info('Channel exe: ');
-  log.info(autoUpdater.channel);
+  log.info(`Version App: ${app.getVersion()}`);
+  log.info(`Channel: ${autoUpdater.channel}`);
+
 
   createWindow();
   updaterListeners();
   const resultUpdater = await autoUpdater.checkForUpdatesAndNotify();
+
+  /* log.info('ResultUpdater: ');
+  log.info(resultUpdater); */
+
   if (resultUpdater !== null) {
     if (resultUpdater.versionInfo.version !== app.getVersion()) {
       return;
